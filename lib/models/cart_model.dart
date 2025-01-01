d925b4015d1ffb12a3ff9a896b9d080bb9c7bca2
[KasunAmarasingha] <[kasunsamudika020@gmail.com]>
2025-01-01 17:21:41 +0530

    Create cart of the marketplace

--- lib/models/cart_model.dart
+++ lib/models/cart_model.dart
@@ -1,21 +1,74 @@
-// lib/models/cart_model.dart
 import 'package:flutter/material.dart';
 import 'package:srifitness_app/pages/marketplace/product_model.dart';
 
-class Cart with ChangeNotifier {
-  final List<Product> _items = [];
+class CartItem {
+  final Product product;
+  int quantity;
+  bool selected;
 
-  List<Product> get items => _items;
+  CartItem({required this.product, this.quantity = 1, this.selected = false});
+}
+
+class Cart with ChangeNotifier {
+  final List<CartItem> _items = [];
+
+  List<CartItem> get items => _items;
 
   void addProduct(Product product) {
-    _items.add(product);
+    final index = _items.indexWhere((item) => item.product == product);
+    if (index != -1) {
+      _items[index].quantity++;
+    } else {
+      _items.add(CartItem(product: product));
+    }
     notifyListeners();
   }
 
   void removeProduct(Product product) {
-    _items.remove(product);
+    final index = _items.indexWhere((item) => item.product == product);
+    if (index != -1) {
+      _items.removeAt(index);
+    }
     notifyListeners();
   }
 
-  int get itemCount => _items.length;
+  void increaseQuantity(Product product) {
+    final index = _items.indexWhere((item) => item.product == product);
+    if (index != -1) {
+      _items[index].quantity++;
+    }
+    notifyListeners();
+  }
+
+  void decreaseQuantity(Product product) {
+    final index = _items.indexWhere((item) => item.product == product);
+    if (index != -1 && _items[index].quantity > 1) {
+      _items[index].quantity--;
+    } else {
+      _items.removeAt(index);
+    }
+    notifyListeners();
+  }
+
+  void selectItem(Product product, bool selected) {
+    final index = _items.indexWhere((item) => item.product == product);
+    if (index != -1) {
+      _items[index].selected = selected;
+    }
+    notifyListeners();
+  }
+
+  void removeSelectedItems() {
+    _items.removeWhere((item) => item.selected);
+    notifyListeners();
+  }
+
+  void clearCart() {
+    _items.clear();
+    notifyListeners();
+  }
+
+  double get totalPrice {
+    return _items.fold(0.0, (sum, item) => sum + item.product.discountedPrice * item.quantity);
+  }
 }
